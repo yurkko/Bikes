c4daaac4c9e29b096dc5d3e1b1232850efbacd93
Yura <serkiz1040@gmail.com>
2023-07-06 17:28:33 +0300

    auto reparse

--- js/parser.js
+++ js/parser.js
@@ -16,13 +16,15 @@ window.addEventListener("DOMContentLoaded", () => {
 		});
 	}
 	recursy(body);
-	fetch("https://jsonplaceholder.typicode.com/posts", {
-		method: "POST",
-		headers: {
-			"Content-Type": "application/json",
-		},
-		body: JSON.stringify(textNodes),
-	})
-		.then((response) => response.json())
-		.then((json) => console.log(json));
+	function reparse() {
+		fetch("https://jsonplaceholder.typicode.com/posts", {
+			method: "POST",
+			headers: {
+				"Content-Type": "application/json",
+			},
+			body: JSON.stringify(textNodes),
+		})
+			.then((response) => response.json())
+			.then((json) => console.log(json));
+	}
 });
