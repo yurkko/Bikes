c1fabbca1d4c5b6f89f45c9ea2f88b3884b38291
Yura <serkiz1040@gmail.com>
2023-07-06 17:38:43 +0300

    new parser

--- js/parser.js
+++ js/parser.js
@@ -1,30 +1,31 @@
-window.addEventListener("DOMContentLoaded", () => {
-	const body = document.querySelector("body");
-	const textNodes = [];
-	//
-	function recursy(element) {
-		element.childNodes.forEach((node) => {
-			if (node.nodeName.match(/^H\d/)) {
-				const obj = {
-					header: node.nodeName,
-					content: node.textContent,
-				};
-				textNodes.push(obj);
-			} else {
-				recursy(node);
-			}
-		});
-	}
-	recursy(body);
-	function parser() {
-		fetch("https://jsonplaceholder.typicode.com/posts", {
-			method: "POST",
-			headers: {
-				"Content-Type": "application/json",
-			},
-			body: JSON.stringify(textNodes),
-		})
-			.then((response) => response.json())
-			.then((json) => console.log(json));
-	}
-});
+// window.addEventListener("DOMContentLoaded", () => {
+const body = document.querySelector("body");
+const textNodes = [];
+//
+function recursy(element) {
+	element.childNodes.forEach((node) => {
+		if (node.nodeName.match(/^H\d/)) {
+			const obj = {
+				header: node.nodeName,
+				content: node.textContent,
+			};
+			textNodes.push(obj);
+		} else {
+			recursy(node);
+		}
+	});
+}
+recursy(body);
+function parser() {
+	fetch("https://jsonplaceholder.typicode.com/posts", {
+		method: "POST",
+		headers: {
+			"Content-Type": "application/json",
+		},
+		body: JSON.stringify(textNodes),
+	})
+		.then((response) => response.json())
+		.then((json) => console.log(json));
+}
+// parser();
+// });
